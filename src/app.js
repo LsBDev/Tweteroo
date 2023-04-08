134c4245a7bd5ee768630ff86cba50723acbd3a0
Luís Felipe <prof.luifel@gmail.com>
2023-04-08 18:18:08 -0300

    requisições get

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+const PORT = 5000;
+const users = [];
+const tweets = [];
+
+app.post("/sign-up", (req, res) => {
+    const {username , avatar} = req.body;
+    const user = {username: username, avatar: avatar};
+    users.push(user);
+    return res.send("OK");
+})
+
+app.post("/tweets", (req, res) => {
+    const {username , tweet} = req.body;
+    const newTweet = {username: username, tweet: tweet};
+    if(!username || username === "") {
+        return res.send("UNAUTHORIZED");
+    }
+    tweets.push(newTweet);
+    res.send("OK");
+})
+
+app.get("/tweets", (req, res) => {
+        
+    
+})
+
+
+app.listen(PORT, () => console.log(`Rodando na porta: ${PORT}`))
